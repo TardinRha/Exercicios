bce41406443bdd63a0dec2b526ab6aba0e318174
Rhaissa Tardin <rha.tardin@gmail.com>
2022-12-02 20:17:21 -0300

    Exercicio1014

---
+++ Exercicio1014/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+// Exercicio 1014
+//Calcular o consumo médio de um automóvel sendo fornecidos a distância total percorrida em Km,
+//e o total de combustível gasto em Litros.
+//Entrada contém dois valores: um inteiro representando a distância total percorrida em Km,
+// e um valor real representando o total de combustível gasto, com um dígito após o ponto decimal.
+
+{
+    int DistanciaPercorrida = int.Parse(Console.ReadLine());
+    double CombustivelGasto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    double ConsumoMedio = DistanciaPercorrida / CombustivelGasto;
+
+    Console.WriteLine();
+    Console.WriteLine(ConsumoMedio.ToString("F3", CultureInfo.InvariantCulture) + " Km/l");
+    Console.ReadLine();
+}
