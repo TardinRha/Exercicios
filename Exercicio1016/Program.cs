a29e2cf49e5daa37f2a4183faff993dd12225f80
Rhaissa Tardin <rha.tardin@gmail.com>
2022-12-04 12:17:06 -0300

    Exercicio1016

---
+++ Exercicio1016/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+//Exercicio1016
+//Dois carros (X e Y) partem em uma mesma direção.
+//O carro X sai com velocidade constante de 60 Km/h e o carro Y sai com velocidade constante de 90 Km/h.
+//m uma hora (60 minutos) o carro Y consegue se distanciar 30 quilômetros do carro X,
+//ou seja, consegue se afastar um quilômetro a cada 2 minutos.
+//Leia a distância (em Km) e calcule quanto tempo leva (em minutos)
+//para o carro Y tomar essa distância do outro carro.
+//O arquivo de entrada contém um número inteiro.
+//Imprima o tempo necessário seguido da mensagem "minutos".
+
+{
+    int Distancia = int.Parse(Console.ReadLine());
+    int TempoParaDistanciar = Distancia * 2;
+
+    Console.WriteLine();
+    Console.WriteLine(TempoParaDistanciar + " minutos");
+    Console.ReadLine();
+}
