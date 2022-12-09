97d8474c7ae00a0265bcd56eea8f0215b2d61648
Rhaissa Tardin <rha.tardin@gmail.com>
2022-12-09 11:28:05 -0300

    Exercicio1019

---
+++ Exercicio1019/Program.cs
@@ -0,0 +1,17 @@
+﻿//Exercicio 1019
+//Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica,
+//e informe-o expresso no formato horas:minutos:segundos.
+//Imprima o tempo lido no arquivo de entrada (segundos), convertido para horas:minutos:segundos.
+// 1 Hora = 3600 segundos.
+
+{
+    int TempoSegundos = int.Parse(Console.ReadLine());
+    int Horas = TempoSegundos / 3600;
+    int Minutos = (TempoSegundos % 3600) / 60;
+    int Segundos = (TempoSegundos % 3600) % 60;
+
+    Console.WriteLine(" ");
+    Console.WriteLine(Horas + ":" + Minutos + ":" + Segundos);
+    Console.ReadLine();
+}
+
