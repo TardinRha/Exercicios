7b3f71d786979d53f28da117ba53e49bd44a1700
Rhaissa Tardin <rha.tardin@gmail.com>
2022-12-11 18:22:19 -0300

    Exercicio1020

---
+++ Exercicio1020/Program.cs
@@ -0,0 +1,16 @@
+﻿//Exercicio 1020
+//Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a em anos, meses e dias.
+
+{
+    int IdadeEmDias = int.Parse(Console.ReadLine());
+    int IdadeAnos = IdadeEmDias / 365;
+    int IdadeMeses = (IdadeEmDias % 365) / 30;
+    int IdadeDias = (IdadeEmDias % 365) % 30;
+
+    Console.WriteLine(" ");
+    Console.WriteLine(IdadeAnos + " ano (s).");
+    Console.WriteLine(IdadeMeses + " mês (es).");
+    Console.WriteLine(IdadeDias + " dia (s).");
+    Console.ReadLine();
+}
+
