7a0c63030c864f117c88baadbf799ef0b892a7b1
Rhaissa Tardin <rha.tardin@gmail.com>
2023-01-02 21:56:20 -0300

    Exercicio1044

---
+++ Exercicio1044/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// Exercicio 1044
+// Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos",
+// indicando se os valores lidos são múltiplos entre si.
+
+{
+    string[] vet = Console.ReadLine().Split(" ");
+    int A = int.Parse(vet[0]);
+    int B = int.Parse(vet[1]);
+
+    Console.WriteLine(" ");
+    if ((B / A) * A == B)
+    {
+        Console.WriteLine("São múltiplos");
+    }
+    else
+    {
+        Console.WriteLine("Não são múltiplos");
+    }
+}
