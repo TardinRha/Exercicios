60bc298b755cbe2591c8accfe35d4d4220a4b430
Rhaissa Tardin <rha.tardin@gmail.com>
2023-02-02 16:12:24 -0300

    Exercicio1154

--- Exercicio1154/Exercicio1154/Program.cs
+++ Exercicio1154/Exercicio1154/Program.cs
@@ -1,4 +1,5 @@
-﻿// Faça um algoritmo para ler um número indeterminado de dados, contendo cada um, a idade de um indivíduo.
+﻿// Exercicio 1154
+// Faça um algoritmo para ler um número indeterminado de dados, contendo cada um, a idade de um indivíduo.
 // O último dado, que não entrará nos cálculos, contém o valor de idade negativa.
 // Calcular e imprimir a idade média deste grupo de indivíduos.
 
