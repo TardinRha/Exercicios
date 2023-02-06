38cd0d763c8337240f5621ba203e25f96d041594
Rhaissa Tardin <rha.tardin@gmail.com>
2023-02-06 18:06:07 -0300

    Exercicio1080

---
+++ Exercicio1080/Exercicio1080/Program.cs
@@ -0,0 +1,24 @@
+﻿// Exercicio 1080
+// Leia 10 valores inteiros. Apresente então o maior valor lido e a posição dentre os 100 valores lidos.
+
+{
+    int N = 0;
+    int maior = 0;
+    int linha = 0;
+
+    for (int i = 1; i <= 10; i++)
+    {
+        N = int.Parse(Console.ReadLine());
+
+        if (N > maior)
+        {
+            maior = N;
+            linha = i;
+        }
+    }
+
+    Console.WriteLine();
+    Console.WriteLine(maior);
+    Console.WriteLine(linha);
+    Console.WriteLine();
+}
