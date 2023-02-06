4ac0bad536c01343b64dca2973c18c59986dd758
Rhaissa Tardin <rha.tardin@gmail.com>
2023-02-06 18:05:31 -0300

    Exercicio1075

---
+++ Exercicio1075/Exercicio1075/Program.cs
@@ -0,0 +1,16 @@
+﻿// Exercicio 1075
+// Leia um valor inteiro N. Apresente todos os números entre 1 e 10000 que divididos por N dão resto igual a 2.
+
+{
+    int N = int.Parse(Console.ReadLine());
+    int resto;
+
+    for (int i = 1; i < 1000; i++)
+    {
+        resto = i % N;
+        if (resto == 2)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
