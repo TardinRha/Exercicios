4fb470200d6c331184091e33ea4fd2ebb0369dce
Rhaissa Tardin <rha.tardin@gmail.com>
2023-02-13 11:17:39 -0300

    Exercicio1133

---
+++ Exercicio1133/Exercicio1133/Program.cs
@@ -0,0 +1,35 @@
+﻿// Exercicio 1133
+// Escreva um programa que leia 2 valores X e Y e que imprima todos os valores entre eles cujo resto da divisão dele
+// por 5 for igual a 2 ou igual a 3.
+
+
+{
+    int i;
+
+    int x = int.Parse(Console.ReadLine());
+    int y = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+
+    if (x < y)
+    {
+        for (i = x++; i < y; i++)
+        {
+            if (i % 5 == 2 || i % 5 == 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+    else
+    {
+        for (i = y++; i < x; i++)
+        {
+            if (i % 5 == 2 || i % 5 == 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    Console.WriteLine();
+}
