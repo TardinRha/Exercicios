8757cdc075e6f4b072fbc787f4251b7da5345b4d
Rhaissa Tardin <rha.tardin@gmail.com>
2023-02-14 18:11:44 -0300

    Exercicio1142

---
+++ Exercicio1142/Exercicio1142/Program.cs
@@ -0,0 +1,20 @@
+﻿// Exercicio 1142
+// Escreva um programa que leia um valor inteiro N.
+// Este N é a quantidade de linhas de saída que serão apresentadas na execução do programa.
+
+
+{
+    int N = int.Parse(Console.ReadLine());
+    int X = 0;
+
+    for (int i = 1; i <= N; i++)
+    {
+        for (int n = 1; n <= 3; n++)
+        {
+            Console.Write($"{++X} ");
+        }
+        Console.WriteLine("PUM");
+        X++;
+    }
+    Console.WriteLine();
+}
