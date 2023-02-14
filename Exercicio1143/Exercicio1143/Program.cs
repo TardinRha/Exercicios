657ba8f0664c171ce92a248b59c3deef998c24e3
Rhaissa Tardin <rha.tardin@gmail.com>
2023-02-14 18:14:49 -0300

    Exercicio1143

---
+++ Exercicio1143/Exercicio1143/Program.cs
@@ -0,0 +1,15 @@
+﻿// Exercicio 1143
+// Escreva um programa que leia um valor inteiro N (1 < N < 1000).
+// Este N é a quantidade de linhas de saída que serão apresentadas na execução do programa.
+
+{
+    int N = int.Parse(Console.ReadLine());
+
+    for (int i = 1; i <= N; i++)
+    {
+        Console.Write($"{i} ");
+        Console.Write($"{i * i} ");
+        Console.WriteLine((i * i) * i);
+    }
+    Console.WriteLine();
+}
