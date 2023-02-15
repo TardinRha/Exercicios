6fd1fda16d4436851443cc7a22a3109ced7b1047
Rhaissa Tardin <rha.tardin@gmail.com>
2023-02-15 16:16:03 -0300

    Exercicio1144

---
+++ Exercicio1144/Exercicio1144/Program.cs
@@ -0,0 +1,22 @@
+﻿// Exercicio 1144
+// Escreva um programa que leia um valor inteiro N. N * 2 linhas de saída serão apresentadas na execução do programa,
+// seguindo a lógica do exemplo abaixo. Para valores com mais de 6 dígitos, todos os dígitos devem ser apresentados.
+
+
+{
+    int N = int.Parse(Console.ReadLine());
+
+    for (int i = 1; i <= N; i++)
+    {
+        Console.Write($"{i} ");
+        Console.Write($"{i * i} ");
+        Console.WriteLine((i * i) * i);
+
+        Console.Write($"{i} ");
+        Console.Write($"{(i * i) + 1} ");
+        Console.WriteLine(((i * i) * i) + 1);
+
+    }
+    Console.WriteLine();
+
+}
